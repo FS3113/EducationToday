92151422f2a87d8f3e6d650c6d4e070bbaebbf11
Juefei Chen <juefei@Juefeis-MacBook-Pro.local>
2020-09-05 20:50:31 -0500

    update

--- get_data_from_multiple_lists.py
+++ get_data_from_multiple_lists.py
@@ -768,7 +768,7 @@ def view_html_structure(url, scrape_option, known_html=[], wrong_words=[]):
     return final_result
 
 
-a = view_html_structure('https://mcb.illinois.edu/faculty/by_research', 'urllib')
+a = view_html_structure('https://cs.illinois.edu/about/people/all-faculty', 'urllib')
 print(len(a))
 for i in a:
     print(i)
